9d861725805ff67b2af6247e822c1f641150f712
lucy <lucyperopero@gmail.com>
2022-05-22 22:57:22 -0300

    Input bugfix

--- src/main.rs
+++ src/main.rs
@@ -57,6 +57,7 @@ fn fill_input(kb: &mut [u8]) {
     kb[0x8] = is_key_down(KeyCode::S) as u8;
     kb[0x9] = is_key_down(KeyCode::D) as u8;
     kb[0xA] = is_key_down(KeyCode::Z) as u8;
+    kb[0xB] = is_key_down(KeyCode::C) as u8;
     kb[0xC] = is_key_down(KeyCode::Key4) as u8;
     kb[0xD] = is_key_down(KeyCode::R) as u8;
     kb[0xE] = is_key_down(KeyCode::F) as u8;
@@ -65,8 +66,6 @@ fn fill_input(kb: &mut [u8]) {
 
 #[macroquad::main(get_mq_conf)]
 async fn main() {
-   
-    
     //TODO(lucypero): consider Chip8::from_rom() instead of new() and load_rom()
     let mut chip = lib::Chip8::new(lib::Computer::Normal);
 
@@ -103,7 +102,7 @@ async fn main() {
             chip.tick();
 
             // if chip.sreg > 0 {
-            //     
+            //
             // } else {
             // }
         }
